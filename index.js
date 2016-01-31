d02a6f8a1aba24cef12eff21a8009a325218d708
riukkie <yohlane@gmail.com>
2016-01-31 19:08:41 +0100

    Add plugin archi and hook on user connection

---
+++ index.js
@@ -0,0 +1,11 @@
+(function() {
+	"use strict";
+
+	var OssAuth = {
+		userConnect: function(data, callback) {
+				console.log("[OssAuth] - userConnect");
+			}
+	};
+
+	module.exports = OssAuth;
+})();
